1bf3df04565dadeb7facfe20e5a1931994233bb4
demi zhao <demizhao@demis-MacBook-Pro.local>
2020-04-04 10:45:36 -0400

    Updates

---
+++ precache-manifest.6fbbe375765c74375bd6e04f3273bb49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b806eff607acdd7a8cfec61d8d82913f",
+    "url": "/react-hook-app/index.html"
+  },
+  {
+    "revision": "edec9dd4920c657305da",
+    "url": "/react-hook-app/static/js/2.5c986e82.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/react-hook-app/static/js/2.5c986e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955c150e04a9479b20c1",
+    "url": "/react-hook-app/static/js/main.a2a46af9.chunk.js"
+  },
+  {
+    "revision": "9d8b675cae4b02c4ebab",
+    "url": "/react-hook-app/static/js/runtime-main.9a41dee3.js"
+  }
+]);
